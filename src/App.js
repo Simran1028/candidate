140aac128cddc7efd2f10c1e84bdbc042185d10b
Simran1028 <simrandeepkaur06488@gmail.com>
2024-10-17 23:19:36 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+export default function App() {
+  const [email, setEmail] = useState('')
+  const [input, setInput] = useState([])
+
+  const enter = () => {
+    if (email.includes('@gmail.com')) {
+      alert('Email is valid!')
+
+      const newEmail = {
+        id: input.length + 1,
+        title: email,
+
+      };
+      setInput([...input, newEmail])
+      setEmail('')
+
+    }
+    else if (email.includes(' ')) {
+      alert('Email is invalid!')
+      setEmail('')
+    }
+    else {
+      alert('Email is invalid!')
+      setEmail('')
+    }
+
+  }
+
+  const deleteEmail = (id) => {
+    setInput(input.filter((input) => input.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <div className='container'>
+        <div className='heading'>Add candidate</div>
+        <div className='box'>
+          <div className='output'>
+            <div className='result'>
+              <ul className='list'>
+                {
+                  input.map((email) => (
+                    <li key={email.id} >
+                      <span> {email.title} </span>
+                      <button onClick={() => deleteEmail(email.id)}>X</button>
+                    </li>
+                  ))
+                }
 
-export default App;
+              </ul>
+
+            </div>
+
+          </div>
+          <div>
+
+            <input type='email' placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+        </div>
+        <div className='Enter'><button onClick={enter}>Enter</button></div>
+      </div>
+    </div>
+  )
+
+}
